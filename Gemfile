9c75b51279d10db39b28f5de205d5285ac629edc
zfz <zhngfz@gmail.com>
2015-03-29 16:18:50 +0000

    Add and setup acts_as_votable

--- Gemfile
+++ Gemfile
@@ -40,3 +40,4 @@ gem 'spring',        group: :development
 
 gem 'devise', '~> 3.3.0'
 gem 'bootstrap-sass', '~> 3.2.0.2'
+gem 'acts_as_votable', '~> 0.10.0'
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -65,6 +65,18 @@ class LinksController < ApplicationController
     end
   end
 
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by(current_user)
+    redirect_to :back 
+  end
+
+  def downvote
+    @link = Lin.find(params[:id])
+    @link.downvote_by(current_user)
+    redirect_to :back
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
