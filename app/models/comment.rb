64c05694fd9c60b08cb13bad435e5e3204042fa7
zfz <zhngfz@gmail.com>
2015-04-20 13:08:23 +0000

    Validates link and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+  validates :body, presence: true
+
   belongs_to :user
   belongs_to :link
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ActiveRecord::Base
+  validates :title, :url, presence: true
+
   acts_as_votable
   belongs_to :user
   has_many :comments
