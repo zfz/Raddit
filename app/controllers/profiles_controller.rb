1ead89ee5fb67ce83ba3773467930ec95171525b
zfz <zhngfz@gmail.com>
2015-04-27 06:09:44 +0000

    Profile page to show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,11 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by_name(params[:id])
+    if @user
+      @links = Link.where(user_id: @user.id)
+      render action: :show
+    else
+      render file: 'public/404', status: 404, formats: [:html]
+    end
   end
 end
